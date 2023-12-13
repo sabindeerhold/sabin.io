d15c4ef34b40d41845072c805c424167fdf0c092
sabindeerhold <ssimkhada@deerhold.org>
2023-12-14 00:51:33 +0545

    update chat

--- src/pages/livechat.jsx
+++ src/pages/livechat.jsx
@@ -1,21 +1,14 @@
-// import React, { useEffect } from "react";
 import React from "react";
-import { useEffect } from "react";
+import TawkTo from "@tawk.to/tawk-messenger-react";
 
 const LiveChat = () => {
-	useEffect(() => {
-		const tawkToScript = document.createElement("script");
-		tawkToScript.src =
-			"https://embed.tawk.to/6579f29a07843602b801bc5b/default";
-		tawkToScript.async = true;
-		document.body.appendChild(tawkToScript);
-
-		return () => {
-			document.body.removeChild(tawkToScript);
-		};
-	}, []);
-
-	return <div></div>; // You can customize this placeholder div
+	return (
+		<TawkTo
+			accountId="6579f29a07843602b801bc5b"
+			chatId="1hhi5nbcv"
+			loading={<div>Loading chat...</div>}
+		/>
+	);
 };
 
 export default LiveChat;
